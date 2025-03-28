0b408a32cfbcef8552dfc55b1019dfa4308d4a96
Fayzy <fayzhan.khan19@gmail.com>
2025-03-28 00:43:38 +0000

    Updated gitignore

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAa-eHOk9R91o87-1uM95dWD0w0Xu-JcYk",
+  authDomain: "chittrapi-715ea.firebaseapp.com",
+  projectId: "chittrapi-715ea",
+  storageBucket: "chittrapi-715ea.firebasestorage.app",
+  messagingSenderId: "89645458348",
+  appId: "1:89645458348:web:efbaf639a173b4306f5009"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
